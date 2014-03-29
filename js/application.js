334953ad02462c61e8f97ff236018b03a4f72484
jluyke <jordanluyke@gmail.com>
2014-03-29 15:57:28 -0700

    added server status

--- js/application.js
+++ js/application.js
@@ -1,4 +1,12 @@
 $(document).ready(function() {
   polychroma.init("my-canvas-id");
   polychroma.connect("http://54.186.198.160:5000");
+
+  setTimeout(function() {
+    if (polychroma.isConnected()) {
+      $("#server-status").html("Connected! Try painting with friends!")
+    } else {
+      $("#server-status").html("Not connected.")
+    }
+  }, 1000);
 });
