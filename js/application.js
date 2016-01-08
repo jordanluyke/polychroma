e078f3af51c22f0b94ae823c72eba33e03cc62dc
Jordan Luyke <jordanluyke@gmail.com>
2016-01-08 10:51:50 -0800

    retina support

--- js/application.js
+++ js/application.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
-  polychroma.init("my-canvas-id");
-  polychroma.connect("http://54.187.149.115:5000");
+  polychroma.init('my-canvas-id', 640, 400);
+  polychroma.connect('http://54.187.149.115:5000');
 
   setTimeout(function() {
     if (polychroma.isConnected()) {
-      $("#server-status").html("Connected! Try painting with friends!");
+      $('#server-status').html('Connected! Try painting with friends!');
     } else {
-      $("#server-status").html("Not connected.");
+      $('#server-status').html('Not connected.');
     }
   }, 1000);
 });
